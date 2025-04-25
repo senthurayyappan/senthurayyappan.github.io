b79fcced82256a5b27dd3354e04b3a7011b1537e
imsenthur <imsenthur@gmail.com>
2025-04-25 10:41:38 -0400

    feat: about page is now structured

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -12,19 +12,28 @@ export default function Page() {
     <div className="comic grid grid-cols-2 md:grid-cols-3 gap-2 grid-rows-[minmax(200px,1fr)_minmax(200px,1fr)_auto_auto]">
       {/* Header Panel */}    
       <ComicPanel description="Senthur Ayyappan, Research Engineer @ Neurobionics Lab, U-M" className='col-span-2 row-span-2' titlePosition='bottom-right' imageSrc='/about/header.jpg'>
-        <div className='speech' style={{position: 'absolute', top: '10%', right: '10%', display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
+        <div className='speech' style={{position: 'absolute', top: '8%', right: '5%', display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
           <p className='font-bold'>Gym Badges</p>
           
-          <div className="flex flex-col gap-2 justify-center py-4">
-            <Link href="https://github.com/senthurayyappan" target="_blank" className="rounded-full border-4 border-transparent hover:border-black transition-colors">
-              <Image src="/icons/github.png" alt="GitHub" width={32} height={32} />
+          <div className="grid grid-cols-3 grid-rows-2 gap-2 p-2">
+            <Link href="https://www.nitt.edu/" target="_blank" className="rounded-full panel-button">
+              <Image src="/nitt.png" alt="LinkedIn" width={48} height={48} className='p-1'/>
             </Link>
-            <Link href="https://www.linkedin.com/in/imsenthur/" target="_blank" className="rounded-full border-4 border-transparent hover:border-black transition-colors">
-              <Image src="/icons/linkedin.png" alt="LinkedIn" width={32} height={32} />
+            <Link href="https://www.iitm.ac.in/" target="_blank" className="rounded-full panel-button">
+              <Image src="/iitm.png" alt="IIT Madras" width={48} height={48} className='p-1'/>
             </Link>
-            <Link href="https://www.instagram.com/senthurayyappan/" target="_blank" className="rounded-full border-4 border-transparent hover:border-black transition-colors">
-              <Image src="/icons/instagram.png" alt="Instagram" width={32} height={32} />
+            <Link href="https://www.drdo.gov.in/" target="_blank" className="rounded-full panel-button">
+              <Image src="/drdo.png" alt="DRDO" width={48} height={48} className='p-1'/>
             </Link>
+            <Link href="https://www.nsf.gov/" target="_blank" className="rounded-full panel-button">
+              <Image src="/nsf.png" alt="NSF" width={48} height={48} />
+            </Link>            
+            <Link href="https://robotics.umich.edu/" target="_blank" className="panel-button">
+              <Image src="/mrobotics.png" alt="Michigan Robotics" width={48} height={48} className='p-1'/>
+            </Link>
+            <Link href="https://www.rai-inst.com/" target="_blank" className="panel-button">
+              <Image src="/rai.jpg" alt="RAI" width={48} height={48} className='p-1'/>
+            </Link>             
           </div>
         </div>
       </ComicPanel>
@@ -41,9 +50,19 @@ export default function Page() {
 
       <ComicPanel 
         className="col-span-1 row-span-2 h-full" 
+        title="Consultant @ Robotics and AI Institute"
+        titlePosition="top-right"
+        imageSrc='/about/umv.webp'
+        href='https://www.opensourceleg.org/'
+        description='Worked on co-design of the next-generation Ultra-Mobility Vehicle (UMV)'
+      >
+      </ComicPanel>
+
+      <ComicPanel 
+        className="col-span-1 row-span-1 h-full" 
         title="Research Intern @ DRDO | DEBEL"
         imageSrc='/about/exo.jpg'
-        imagePosition='90% 40%'
+        imagePosition='0% 90%'
         titlePosition="top-right"
         href='https://www.drdo.gov.in/drdo/labs-and-establishments/defence-bio-engineering-electro-medical-laboratory-debel'
         description='Worked on 2D modeling and simulation of a lower-limb powered exoskeleton'
@@ -51,7 +70,7 @@ export default function Page() {
       </ComicPanel>
 
       <ComicPanel 
-        className="col-span-2 row-span-1 h-full" 
+        className="col-span-1 row-span-1 h-full" 
         title="Research Intern @ IIT Madras"
         imageSrc='/about/iitm.jpg'
         imagePosition='0% 15%'
@@ -67,7 +86,7 @@ export default function Page() {
         imageSrc='/about/spider.jpg'
         imagePosition='0% 30%'
         titlePosition="top-right"
-        href='https://ed.iitm.ac.in/~robotics_lab/index.html'
+        href='https://spider.nitt.edu/'
         description='Led a team of around 30 self-motivated undergraduate students working on various robotics and machine learning projects'
       >
       </ComicPanel>
@@ -75,38 +94,37 @@ export default function Page() {
       {/* Education Panel */}
       <ComicPanel 
         className="col-span-1 h-full" 
-        title="EDUCATION" 
-        titlePosition="bottom-right"
-        childrenClassName="p-6"
+        title="Education" 
+        titlePosition="top-right"
+        childrenClassName="p-12"
+        description='National Institute of Technology, Tiruchirappalli (NITT)'
       >
-        <div className="space-y-4">
-          <div>
-            <h3 className="font-bold">National Institute of Technology Tiruchirappalli</h3>
-            <p>Bachelor of Technology, Production Engineering</p>
-            <p className="text-sm text-gray-600">JULY 2016 - JUNE 2020</p>
-            <p>Minor degree in Computer Science, CGPA: 8.44/10.0</p>
-          </div>
-          
-          <div>
-            <h3 className="font-bold">Maharishi International Residential School</h3>
-            <p>Central Board of Secondary Education</p>
-            <p className="text-sm text-gray-600">2013 - 2015</p>
-            <p>Computer Science, Percentage: 92.6</p>
-          </div>
+        <div className='flex flex-col gap-0'>
+            <p>BTech, Production Engineering</p>
+            <p>Minor degree in CS</p>
+            <p className='font-bold'>CGPA: 8.44/10.0</p>         
         </div>
       </ComicPanel>
       
+      <ComicPanel imageSrc="sa-publications.jpg" href="/publications">
+          <p className="speech" style={{ position: 'absolute', top: '5%', right: '5%' }}>
+            Publications
+          </p>
+      </ComicPanel>
+      
+      <ComicPanel className="col-span-1 md:row-span-2" imageSrc="/projects/ballbot-main.jpg" title="Projects" titlePosition='bottom-right' href="/projects"/>
+
       {/* Skills Panel */}
       <ComicPanel 
         className="col-span-1 md:col-span-2 h-full" 
-        title="SKILLS" 
+        title="Skills" 
         titlePosition="bottom-right"
-        childrenClassName="p-6"
+        childrenClassName="p-12"
       >
-        <div className="space-y-2">
-          <p><span className="font-bold">Programming Language:</span> Python, C, C#, Embedded Linux, Javascript</p>
-          <p><span className="font-bold">Design:</span> Solidworks, Blender, Fusion360, Onshape</p>
-          <p><span className="font-bold">Other Packages:</span> ROS, Gazebo</p>
+        <div className='flex flex-col gap-2'>
+          <p><span className="font-bold">Programming Languages:</span> Python, C++, Rust (Novice), C#, Javascript, TSX</p>
+          <p><span className="font-bold">Design:</span> Onshape, Solidworks, Fusion360, Blender</p>
+          <p><span className="font-bold">Simulation / Visualization:</span> Gazebo, MuJoCo, V-REP (CoppeliaSim), Rviz</p>
         </div>
       </ComicPanel>
       
